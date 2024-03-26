c06f23ddc25f8c56eda3f536b69275a14335c4c1
MuratALTUNALAN61 <you@example.commurat_altunalan370@hotmail.com>
2024-03-26 20:31:10 +0300

    veri tabanından yetki geldi

--- Form1.cs
+++ Form1.cs
@@ -1,3 +1,5 @@
+using System.Data.SqlClient;
+
 namespace HBYS
 {
     public partial class Form1 : Form
@@ -7,8 +9,28 @@ namespace HBYS
             InitializeComponent();
         }
 
+        SqlConnection baglanti = new SqlConnection("Data Source=DESKTOP-8J8VAOJ\\SQLEXPRESS03;Initial Catalog=HBYS;Integrated Security=SSPI");
+
         private void buttonGirisYap_Click(object sender, EventArgs e)
         {
+            string parola;
+            baglanti.Open();
+            SqlCommand komut1 = new SqlCommand("select kullanici_id from Kullanici where kullaniciAdi=@isim and sifre=@sifre", baglanti);
+            komut1.Parameters.AddWithValue("@isim",textBoxKullaniciAdi.Text);
+            komut1.Parameters.AddWithValue("sifre", textBoxSifre.Text);
+            SqlDataReader dr = komut1.ExecuteReader();
+            dr.Read();
+            int id = (int)dr["kullanici_id"];
+            dr.Close();
+
+            SqlCommand komut2 = new SqlCommand("select Yetki.yetki from Yetki join Kullanici_yetki on Yetki.yetki_id=Kullanici_yetki.y_id where Kullanici_yetki.k_id=@id",baglanti);
+            komut2.Parameters.AddWithValue("@id", id);
+            SqlDataReader dr2= komut2.ExecuteReader();
+            dr2.Read();
+            string yetki = (string)dr2["yetki"];
+            
+            
+
             FormSekreter yeniSekreter = new FormSekreter();
             yeniSekreter.Show();
             this.Hide();
--- obj/Debug/net8.0-windows/HBYS.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/HBYS.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HBYS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ce9be557d0c8a079138b2ffb9f32155001156a3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bcf66062ffe71ec9bde52416fb4ad4adc56f8b43")]
 [assembly: System.Reflection.AssemblyProductAttribute("HBYS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HBYS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
